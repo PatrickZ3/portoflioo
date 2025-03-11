c8459aec29f088e3c93f46fbb91dcab21332a154
Patrick <68784059+PatrickZ3@users.noreply.github.com>
2025-03-11 14:17:16 -0400

    css adjust

---
+++ .next/static/chunks/src_app_page_tsx_8b298505._.js
@@ -0,0 +1,18 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_page_tsx_8b298505._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_page_tsx_8b298505._.js",
+  "chunks": [
+    "static/chunks/node_modules_next_3085a989._.js",
+    "static/chunks/node_modules_react-icons_fa_index_mjs_d2e2d7f5._.js",
+    "static/chunks/node_modules_react-icons_bs_index_mjs_0b2b1793._.js",
+    "static/chunks/node_modules_react-icons_lib_74ccc930._.js",
+    "static/chunks/node_modules_@fortawesome_free-brands-svg-icons_index_mjs_660f2e06._.js",
+    "static/chunks/node_modules_@fortawesome_free-solid-svg-icons_index_mjs_6d4e8d45._.js",
+    "static/chunks/node_modules_69fd3a8b._.js",
+    "static/chunks/src_app_87543e93._.js"
+  ],
+  "source": "dynamic"
+});
